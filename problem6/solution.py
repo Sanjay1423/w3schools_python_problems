674ebe85603dc7331ef3eda3404819069b4b2a81
Sanjay1423 <sanjayssp1423@gmail.com>
2022-07-06 09:13:18 +0530

    Problem6 added

---
+++ problem6/solution.py
@@ -0,0 +1,13 @@
+numbers = list(input().split(','))
+check_list = []
+sum = 0
+for i in numbers:
+    sum += 1
+    if i not in check_list:
+        check_list.append(i)
+        if sum == len(numbers):
+            print(True)
+            break
+    elif i in check_list:
+        print(False)
+        break
